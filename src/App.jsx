62f11b431f6700d976d608cec853d1fe689de93d
young <hyangyoungs@gmail.com>
2024-07-28 23:07:03 -0500

    feat: sidebar - cities

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,47 @@ import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
+import Login from "./pages/Login.jsx";
+import CityList from "./components/CityList.jsx";
+import {useEffect, useState} from "react";
+
+const BASE_URL = 'http://localhost:9000'
 
 function App() {
+
+    const [cities, setCities] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        async function fetchCities() {
+            try {
+                const res = await fetch(`${BASE_URL}/cities`);
+                console.log(res)
+                const data = await res.json();
+                setCities(data);
+            } catch {
+                alert('There was an error loading data...');
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchCities();
+    }, []);
+
     return (
         <>
-            <h1>Hello Router!</h1>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Homepage/>}/>
+                    <Route index element={<Homepage/>}/>
                     <Route path="product" element={<Product/>}/>
                     <Route path="pricing" element={<Pricing/>}/>
-                    <Route path="app" element={<AppLayout/>}/>
+                    <Route path="/login" element={<Login/>}/>
+                    <Route path="app" element={<AppLayout/>}>
+                        <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                        <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>}/>
+                        <Route path="countries" element={<p>countries</p>}/>
+                        <Route path="form" element={<p>form</p>}/>
+                    </Route>
                     <Route path="*" element={<PageNotFound/>}/>
                 </Routes>
             </BrowserRouter>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,13 @@
-import AppNav from "../components/AppNav.jsx";
+import Sidebar from "../components/Sidebar.jsx";
+import Map from "../components/Map.jsx";
+import styles from './AppLayout.module.css'
 
 function AppLayout() {
     return (
-        <div>
-            <AppNav/>
-            App
+        <div className={styles.app}>
+            <Sidebar/>
+            <Map/>
         </div>
     )
 }
-
 export default AppLayout
