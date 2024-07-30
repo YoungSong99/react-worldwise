43975c704661b2c77734856a56088816b7aabcfb
young <hyangyoungs@gmail.com>
2024-07-30 15:39:18 -0500

    feat: redirection after logout

--- src/contexts/CitiesProvider.jsx
+++ src/contexts/CitiesProvider.jsx
@@ -70,7 +70,6 @@ function CitiesProvider({ children }) {
 
     async function getCity(id) {
         if (Number(id) === currentCity.id) return;
-        dispatch({ type: 'loading' });
         try {
             const res = await fetch(`${BASE_URL}/cities/${id}`);
             const data = await res.json();
