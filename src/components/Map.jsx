97bd07e27b6cb46be10f81177776cfc39f023221
young <hyangyoungs@gmail.com>
2024-07-29 23:47:45 -0500

    feat: marker and city item is synchronized.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,31 +1,53 @@
 import styles from './Map.module.css';
-// import {useNavigate} from 'react-router-dom';
-import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
-import {useState} from "react";
+import {MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents} from "react-leaflet";
+import {useSearchParams} from "react-router-dom";
+import {UseCities} from "../contexts/CitiesProvider.jsx";
+import {useEffect, useState} from "react";
+
 
 function Map() {
-    // const navigate = useNavigate()
-    const [mapPosition] = useState([40, 0])
 
-    // const [searchParams, setSearchParams] = useSearchParams();
-    // const lat = searchParams.get("lat");
-    // const lng = searchParams.get("lng");
+    const {cities} = UseCities()
+    const [mapPosition, setMapPosition] = useState([40, 0])
+    const [searchParams] = useSearchParams()
+    const mapLat = searchParams.get("lat")
+    const mapLng = searchParams.get("lng")
+
+    useEffect(() => {
+        if (mapLat && mapLng) setMapPosition([mapLat, mapLng])
+    }, [mapLat, mapLng]);
+
 
     return (
         <div className={styles.mapContainer}>
-            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+            <MapContainer
+                center={mapPosition}
+                zoom={6} scrollWheelZoom={true}
+                className={styles.map}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
                 />
-                <Marker position={mapPosition}>
+                {cities.map(city =>
+                    <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
                     <Popup>
-                        A pretty CSS3 popup. <br/> Easily customizable.
+                        <span>{city.emoji}</span>
+                        <span>{city.cityName}</span>
                     </Popup>
                 </Marker>
+                )}
+                <ChangeCenter position={mapPosition}/>
             </MapContainer>
         </div>
     );
 }
+
+function ChangeCenter({position}) {
+    const map = useMap()
+    map.setView(position)
+    return null;
+}
+
+
 
 export default Map;
